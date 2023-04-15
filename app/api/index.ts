161e6cc877acc39d51262abd23d3ca56ee31e5f2
Sasha Moak <sasha.moak@gmail.com>
2023-04-15 16:25:06 -0700

    chore: add debugging statements
    there exists an issue where the code in prod isn't behaving the same
    as in dev. Adding debugging statements to determine root cause.

--- app/api/index.ts
+++ app/api/index.ts
@@ -38,6 +38,7 @@ const calculateDaysByDate = (date?: Date) => {
 
 type GetGamesByDate = (date?: Date) => Promise<ScheduledGame[]>;
 export const getGamesByDate: GetGamesByDate = async (date) => {
+  console.log('Getting games by date', date);
   const url = new URL(BASE_URL);
   url.searchParams.append("feed", "modulekit");
   url.searchParams.append("key", CLIENT_KEY);
@@ -47,6 +48,7 @@ export const getGamesByDate: GetGamesByDate = async (date) => {
   const { daysAhead, daysBack } = calculateDaysByDate(date);
   url.searchParams.append("numberofdaysahead", daysAhead);
   url.searchParams.append("numberofdaysback", daysBack);
+  console.log('hitting url', url);
 
   const response = await fetch(url.toString());
   const { SiteKit } = (await response.json()) as ModulekitResponse;
